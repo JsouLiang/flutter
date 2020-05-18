d48bc61e55d30e34188df89efbb87cf8fb29879a
linxuebin <linxuebin.01@bytedance.com>
2020-05-18 19:57:49 +0800

    feat: add a switch to disable mipmaps, save 1/4 GPU memory (BD #59)
    flutter#59

--- bdflutter/lib/ui/performance/performance.h
+++ bdflutter/lib/ui/performance/performance.h
@@ -22,12 +22,17 @@ class Performance {
 
   int64_t GetImageMemoryUsageKB();  // KB
 
+  void DisableMips(bool disable);
+  bool IsDisableMips();
+
   static void RegisterNatives(tonic::DartLibraryNatives* natives);
 
  private:
   Performance();
 
   std::atomic_int64_t dart_image_memory_usage;  // KB
+
+  std::atomic_bool disable_mipmaps_;
 };
 
 }  // namespace flutter
