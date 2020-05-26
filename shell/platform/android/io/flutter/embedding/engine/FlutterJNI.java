e7e2ceb7118a4830e5403b8ad0b0fa7b846d690a
sunkun <sunkun@bytedance.com>
2020-05-26 17:51:33 +0800

    bugfix: fix setSemanticsError

--- shell/platform/android/io/flutter/embedding/engine/FlutterJNI.java
+++ shell/platform/android/io/flutter/embedding/engine/FlutterJNI.java
@@ -821,8 +821,16 @@ public class FlutterJNI {
    */
   @UiThread
   public void setSemanticsEnabled(boolean enabled) {
+    // BD ADD: START
+    if (!isAttached()) {
+      Log.e(TAG, "Cannot execute setSemanticsEnabled because FlutterJNI is not attached to native.");
+      return;
+    }
+    // END
     ensureRunningOnMainThread();
-    ensureAttachedToNative();
+
+    // BD DEL:
+    // ensureAttachedToNative();
     nativeSetSemanticsEnabled(nativeShellHolderId, enabled);
   }
 
