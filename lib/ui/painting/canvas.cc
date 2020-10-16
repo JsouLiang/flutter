056f31c11ef5789a0fba3c82a54e26d3618ed726
chenxuwei.cxw <chenxuwei.cxw@bytedance.com>
2020-10-17 00:42:00 +0800

    fix: add third_parth api macro definition if and config (BD #71)
    1: skia definition config
    2: dart definition config
    
    https://code.byted.org/tech_client/flutter/issues/71

--- lib/ui/painting/canvas.cc
+++ lib/ui/painting/canvas.cc
@@ -485,7 +485,11 @@ void Canvas::Invalidate() {
 
 // BD ADD: START
 bool Canvas::isSkewOrRotate() {
+  #if THIRD_PARTY_SKIA_BD 
   return canvas_ ? canvas_->isSkewOrRotate() : false;
+  #else
+  return false;
+  #endif
 }
 // END
 
