108455b1985df8bc5ebfc90dd1ea806e7394a35c
xieran.sai <xieran.sai@bytedance.com>
2021-01-18 13:04:20 +0000

    Revert "feat:Optimize screenshot memory"
    This reverts commit bf53f699e22dd8a52f4383e541be81cbcd29a979.
    
    
    (cherry picked from commit 550f3140f252f337a2665ca49fd5ed1ccde7b423)

--- shell/platform/android/platform_view_android_jni_impl.cc
+++ shell/platform/android/platform_view_android_jni_impl.cc
@@ -742,7 +742,7 @@ static jobject GetBitmap(JNIEnv* env, jobject jcaller, jlong shell_holder) {
     return nullptr;
   }
 
-  jstring argb = env->NewStringUTF("RGB_565");
+  jstring argb = env->NewStringUTF("ARGB_8888");
   if (argb == nullptr) {
     return nullptr;
   }
