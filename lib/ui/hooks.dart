lfkdsk/engine

feat: load empty UI when shell page exits (BD #23)
flutter#23

lib/ui/hooks.dart
@@ -211,6 +211,14 @@ void _drawFrame() {
   _invoke(window.onDrawFrame, window._onDrawFrameZone);
 }
 
+// BD ADD: START
+@pragma('vm:entry-point')
+// ignore: unused_element
+void _exitApp() {
+  _invoke(window.exitApp, window._exitAppZone);
+}
+// END
+
 // ignore: always_declare_return_types, prefer_generic_function_type_aliases
 typedef _UnaryFunction(Null args);
 // ignore: always_declare_return_types, prefer_generic_function_type_aliases

JsouLiang/flutter

lib/ui/hooks.dart
@@ -120,6 +120,14 @@ void _drawFrame() {
   PlatformDispatcher.instance._drawFrame();
 }
 
+// BD ADD: START
+@pragma('vm:entry-point')
+// ignore: unused_element
+void _exitApp() {
+  _invoke(performance.exitApp, performance._exitAppZone);
+}
+// END
+
 // ignore: always_declare_return_types, prefer_generic_function_type_aliases
 typedef _ListStringArgFunction(List<String> args);
 