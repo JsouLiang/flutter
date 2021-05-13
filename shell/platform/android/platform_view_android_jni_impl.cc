a6ec1c7dd729544612b78d18a06836991c23bd47
zhaoxuyang <zhaoxuyang.6@bytedance.com>
2021-05-13 16:11:59 +0800

    修改打动态化宿主的编译问题

--- shell/platform/android/platform_view_android_jni_impl.cc
+++ shell/platform/android/platform_view_android_jni_impl.cc
@@ -204,19 +204,20 @@ static jobject SpawnJNI(JNIEnv* env,
 }
 
 // BD ADD: START
-static void UpdateSettings(JNIEnv *env, jobject jcaller, jlong shell_holder, jstring package_dill_path, jstring package_preload_libs) {
-    // 每个Holder都拥有自己的Settings，需要设置不同的dynamic_dill_path
-    ANDROID_SHELL_HOLDER->UpdateSettings(fml::jni::JavaStringToString(env, package_dill_path), fml::jni::JavaStringToString(env, package_preload_libs));
-}
-
-static void ScheduleBackgroundFrame(JNIEnv *env, jobject jcaller, jlong shell_holder) {
-    // 每个Holder都拥有自己的Settings，需要设置不同的dynamic_dill_path
-    ANDROID_SHELL_HOLDER->ScheduleBackgroundFrame();
-}
-
-static void ScheduleFrameNow(JNIEnv *env, jobject jcaller, jlong shell_holder) {
-    ANDROID_SHELL_HOLDER->ScheduleFrameNow();
-}
+// TODO ZXY
+//static void UpdateSettings(JNIEnv *env, jobject jcaller, jlong shell_holder, jstring package_dill_path, jstring package_preload_libs) {
+//    // 每个Holder都拥有自己的Settings，需要设置不同的dynamic_dill_path
+//    ANDROID_SHELL_HOLDER->UpdateSettings(fml::jni::JavaStringToString(env, package_dill_path), fml::jni::JavaStringToString(env, package_preload_libs));
+//}
+//
+//static void ScheduleBackgroundFrame(JNIEnv *env, jobject jcaller, jlong shell_holder) {
+//    // 每个Holder都拥有自己的Settings，需要设置不同的dynamic_dill_path
+//    ANDROID_SHELL_HOLDER->ScheduleBackgroundFrame();
+//}
+//
+//static void ScheduleFrameNow(JNIEnv *env, jobject jcaller, jlong shell_holder) {
+//    ANDROID_SHELL_HOLDER->ScheduleFrameNow();
+//}
 // END
 
 static void SurfaceCreated(JNIEnv* env,
@@ -658,21 +659,22 @@ bool RegisterApi(JNIEnv* env) {
           .fnPtr = reinterpret_cast<void*>(&AttachJNI),
       },
       // BD ADD: START
-      {
-              .name = "nativeUpdateSettings",
-              .signature = "(JLjava/lang/String;Ljava/lang/String;)V",
-              .fnPtr = reinterpret_cast<void*>(&UpdateSettings),
-      },
-      {
-              .name = "nativeScheduleBackgroundFrame",
-              .signature = "(J)V",
-              .fnPtr = reinterpret_cast<void*>(&ScheduleBackgroundFrame),
-      },
-      {
-              .name = "nativeScheduleFrameNow",
-              .signature = "(J)V",
-              .fnPtr = reinterpret_cast<void*>(&ScheduleFrameNow),
-      },
+      // TODO ZXY
+//      {
+//              .name = "nativeUpdateSettings",
+//              .signature = "(JLjava/lang/String;Ljava/lang/String;)V",
+//              .fnPtr = reinterpret_cast<void*>(&UpdateSettings),
+//      },
+//      {
+//              .name = "nativeScheduleBackgroundFrame",
+//              .signature = "(J)V",
+//              .fnPtr = reinterpret_cast<void*>(&ScheduleBackgroundFrame),
+//      },
+//      {
+//              .name = "nativeScheduleFrameNow",
+//              .signature = "(J)V",
+//              .fnPtr = reinterpret_cast<void*>(&ScheduleFrameNow),
+//      },
       // END
       {
           .name = "nativeDestroy",
