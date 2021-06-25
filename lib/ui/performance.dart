dac842a7942ad0639b649add9efeb321489fcfaa
周统卫 <zhoutongwei@bytedance.com>
2021-06-25 11:32:18 +0800

    fix: 2.0 nullsafety broken type.
    fix  performance
    
    Signed-off-by: 周统卫 <zhoutongwei@bytedance.com>

--- lib/ui/performance.dart
+++ lib/ui/performance.dart
@@ -53,10 +53,10 @@ class Performance {
    *    index [2] represents number of frames (or 0 in kWorstFpsType mode)
    *    index [3] represents number of dropped frames (or 0 in kWorstFpsType mode)
    */
-  List getFps(int threadType, int fpsType, bool doClear) native 'performance_getFps';
-  int getFpsMaxSamples() native 'Performance_getMaxSamples';
+  List? getFps(int threadType, int fpsType, bool doClear) native 'performance_getFps';
+  int? getFpsMaxSamples() native 'Performance_getMaxSamples';
   void startRecordFps(String key) native 'Performance_startRecordFps';
-  List obtainFps(String key, bool stopRecord) native 'Performance_obtainFps';
+  List? obtainFps(String key, bool stopRecord) native 'Performance_obtainFps';
 
   void startBoost(int flags, int millis) native 'Performance_startBoost';
   void finishBoost(int flags) native 'Performance_finishBoost';
@@ -68,11 +68,11 @@ class Performance {
 
   void stopStackTraceSamples() native 'Performance_stopStackTraceSamples';
 
-  String getStackTraceSamples(int microseconds) native 'Performance_getStackTraceSamples';
+  String? getStackTraceSamples(int microseconds) native 'Performance_getStackTraceSamples';
 
-  bool requestHeapSnapshot(String outFilePath) native 'Performance_requestHeapSnapshot';
+  bool? requestHeapSnapshot(String outFilePath) native 'Performance_requestHeapSnapshot';
 
-  String getHeapInfo() native 'Performance_heapInfo';
+  String? getHeapInfo() native 'Performance_heapInfo';
 
   NotifyIdleCallback? get onNotifyIdle => _onNotifyIdle;
   NotifyIdleCallback? _onNotifyIdle;
@@ -83,24 +83,24 @@ class Performance {
   }
 
   /// Get bitmap cache image info
-  List getSkGraphicCacheMemoryUsage() native 'Performance_skGraphicCacheMemoryUsage';
+  List? getSkGraphicCacheMemoryUsage() native 'Performance_skGraphicCacheMemoryUsage';
 
   /// Get Gpu cache memory.
-  Future<List> getGrResourceCacheMemInfo() {
+  Future<List?> getGrResourceCacheMemInfo() {
     return _futurize(
-          (_Callback<List> callback) => _getGrResourceCacheMemInfo(callback),
+          (_Callback<List?> callback) => _getGrResourceCacheMemInfo(callback),
     );
   }
 
-  String _getGrResourceCacheMemInfo(_Callback<List> callback) native 'Performance_getGpuCacheUsageKBInfo';
+  String? _getGrResourceCacheMemInfo(_Callback<List?> callback) native 'Performance_getGpuCacheUsageKBInfo';
 
   /// Get total ext memory info, contains gpu bitmap image memory and so on.
-  Future<List> getTotalExtMemInfo() {
+  Future<List?> getTotalExtMemInfo() {
     return _futurize(
-          (_Callback<List> callback) => _getTotalExtMemInfo(callback),
+          (_Callback<List?> callback) => _getTotalExtMemInfo(callback),
     );
   }
-  String _getTotalExtMemInfo(_Callback<List> callback) native 'Performance_getTotalExtMemInfo';
+  String? _getTotalExtMemInfo(_Callback<List?> callback) native 'Performance_getTotalExtMemInfo';
   
   /// Get Engine Init APM info
   List<dynamic> getEngineInitApmInfo() native 'Performance_getEngineInitApmInfo';
@@ -108,7 +108,7 @@ class Performance {
 
   void warmUpZeroSizeOnce(bool enable) native 'Performance_warmUpZeroSizeOnce';
 
-  int getRecordLastLayoutTime() native 'Performance_getRecordLastLayoutTime';
+  int? getRecordLastLayoutTime() native 'Performance_getRecordLastLayoutTime';
   void clearHugeFontCache() native 'Performance_clearHugeFontCache';
   void clearAllFontCache() native 'Performance_clearAllFontCache';
   void clearLayoutCache() native 'Performance_clearLayoutCache';
