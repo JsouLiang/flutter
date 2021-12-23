lfkdsk/engine

feat: load empty UI when shell page exits (BD #23)
flutter#23

lib/ui/hooks.dart
@@ -211,6 +211,14 @@ void _drawFrame() {
   _invoke(window.onDrawFrame, window._onDrawFrameZone);
 }
 
+// BD ADD: START
+@pragma('vm:entry-point')
+// ignore: unused_element
+void _exitApp() {
+  _invoke(window.exitApp, window._exitAppZone);
+}
+// END
+
 // ignore: always_declare_return_types, prefer_generic_function_type_aliases
 typedef _UnaryFunction(Null args);
 // ignore: always_declare_return_types, prefer_generic_function_type_aliases

runtime/runtime_controller.cc
@@ -388,6 +388,12 @@ RuntimeController::Locale::~Locale() = default;
 int64_t RuntimeController::GetEngineMainEnterMicros() {
   return client_.GetEngineMainEnterMicros();
 }
+
+void RuntimeController::ExitApp() {
+  if (auto* window = GetWindowIfAvailable()) {
+    window->ExitApp();
+  }
+}
 // END
 
 }  // namespace flutter

JsouLiang/flutter

lib/ui/hooks.dart
@@ -114,6 +114,14 @@ void _drawFrame() {
   PlatformDispatcher.instance._drawFrame();
 }
 
+// BD ADD: START
+@pragma('vm:entry-point')
+// ignore: unused_element
+void _exitApp() {
+  _invoke(performance.exitApp, performance._exitAppZone);
+}
+// END
+
 // ignore: always_declare_return_types, prefer_generic_function_type_aliases
 typedef _ListStringArgFunction(List<String> args);
 

runtime/runtime_controller.cc
@@ -473,6 +473,12 @@ void RuntimeController::AddNextFrameCallback(fml::closure callback) {
 int64_t RuntimeController::GetEngineMainEnterMicros() {
   return client_.GetEngineMainEnterMicros();
 }
+
+void RuntimeController::ExitApp() {
+  if (auto* platform_configuration = GetPlatformConfigurationIfAvailable()) {
+    platform_configuration->ExitApp();
+  }
+}
 // END
 
 }  // namespace flutter