772e177a8c3a6d4f3457edbeed7f61e74f35fcd9
李皓骅 <lihaohua.cafebabe@bytedance.com>
2021-12-27 14:57:52 +0800

    Add latest CI scripts

---
+++ .codebase/auto_roll_engine.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+if [ ! -d "./flutter" ];then
+  git clone git@code.byted.org:tech_client/flutter.git
+fi
+cd flutter
+git checkout -b bd_2.0_pre origin/bd_2.0_pre
+
+cd ../
+git checkout -b bd_2.0 origin/bd_2.0
+
+git log --pretty=format:"%h :%ae  %s  %ad" > a.text
+cur_engine=`head -c 10 a.text`
+old_engine=`head -c 10 ./flutter/bin/internal/ttengine.version`
+
+echo ${cur_engine}
+echo ${old_engine}
+
+#查找出当前engineVersion所在行数
+cur_line=`grep -n ${cur_engine} a.text | cut -d ":" -f 1`
+old_line=`grep -n ${old_engine} a.text | cut -d ":" -f 1`
+if [ $cur_line -lt $old_line ]
+then
+  #更换ttengine.version为最新
+  complete_cur_engine=`git log -1 --pretty=format:"%H"`
+  echo "${complete_cur_engine}"> ./flutter/bin/internal/ttengine.version
+  echo "engine change commits:" >b.txt
+  # mac版本
+  #sed -n "${cur_line},$[old_line-1] p" a.text >>b.txt
+  #linux版本
+  sed -n "${cur_line},$((old_line - 1))p" a.text >>b.txt
+  IFS=
+  message="$(cat b.txt)"
+  echo ${message}
+
+  cd ./flutter
+  git add ./bin/internal/ttengine.version
+  git commit -m "feat: roll engine version to ${complete_cur_engine}" -m "${message}"
+  git push https://ci_flutter:hs7y4aZwT3Ggi9uzx3-X@code.byted.org/tech_client/flutter.git bd_2.0_pre
+fi
+
